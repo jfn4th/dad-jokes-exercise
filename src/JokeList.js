e561ba9c4e7b389a21b730a3c8e7b1d6ec38c3c9
Jamie Nelson <jfn4th@gmail.com>
2020-05-12 12:39:06 -0500

    first commit

---
+++ src/JokeList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Joke from './Joke';
+import styled from 'styled-components';
+import { v4 as uuid } from 'uuid';
+import axios from 'axios';
+
+export default class JokeList extends Component {
+    render() {
+        return <div />;
+    }
+}
