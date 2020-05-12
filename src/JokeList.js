8fce70913ec36c15b5770cc31506bc1e60480ea5
Jamie Nelson <jfn4th@gmail.com>
2020-05-12 16:05:04 -0500

    add initial joke fetchs

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import Joke from './Joke';
 import styled from 'styled-components';
-import { v4 as uuid } from 'uuid';
 import axios from 'axios';
 
+const StyledJokeList = styled.div``;
+
 export default class JokeList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            jokes: []
+        };
+    }
+
+    async componentDidMount() {
+        if (this.state.jokes.length === 0) {
+            let jokes = [];
+            let newJoke;
+            for (let i = 0; i < 10; i++) {
+                do {
+                    const res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+                    newJoke = {
+                        id: res.data.id,
+                        joke: res.data.joke
+                    };
+                } while (jokes.includes(newJoke));
+                jokes = [ ...jokes, newJoke ];
+            }
+            this.setState((st) => ({ jokes: [ ...st.jokes, ...jokes ] }));
+        }
+    }
+
     render() {
-        return <div />;
+        return (
+            <StyledJokeList>
+                <h1>wow</h1>
+            </StyledJokeList>
+        );
     }
 }
