2192870ec8d9cbf14431493dfa2012b9d998acca
Jamie Nelson <jfn4th@gmail.com>
2020-06-02 22:43:53 -0500

    add dynamic vote border and emoji styling

--- src/Joke.js
+++ src/Joke.js
@@ -9,7 +9,7 @@ const StyledJoke = styled.div`
     align-items: center;
     justify-content: center;
     font-weight: 400;
-    padding: 1rem;
+    padding: 1.5rem;
 `;
 const JokeButtons = styled.div`
     display: flex;
@@ -18,6 +18,19 @@ const JokeButtons = styled.div`
     justify-content: center;
     width: 15%;
 `;
+const voteBorderStyles = css`
+    border-color: ${(props) =>
+        props.votes >= 15
+            ? `#4CAF50`
+            : props.votes >= 12
+              ? `#8BC34A`
+              : props.votes >= 9
+                ? `#CDDC39`
+                : props.votes >= 6 ? `#FFEB3B` : props.votes >= 3 ? `#FFC107` : props.votes >= 0 ? `#FF9800` : `#F44336`};
+    border-style: solid;
+    border-width: 3px;
+`;
+
 const JokeVotes = styled.span`
     width: 50px;
     height: 50px;
@@ -26,8 +39,8 @@ const JokeVotes = styled.span`
     text-align: center;
     font-size: 20px;
     font-weight: 300;
-    border: 3px solid red;
     box-shadow: 0 10px 38px rgba(0, 0, 0, 0.2), 0 10px 12px rgba(0, 0, 0, 0.1);
+    ${voteBorderStyles};
 `;
 const JokeText = styled.div`
     width: 75%;
@@ -59,6 +72,7 @@ export default class Joke extends Component {
         super(props);
         this.upVote = this.upVote.bind(this);
         this.downVote = this.downVote.bind(this);
+        this.getEmoji = this.getEmoji.bind(this);
     }
 
     upVote(evt) {
@@ -68,18 +82,30 @@ export default class Joke extends Component {
     downVote(evt) {
         this.props.assessVote(this.props.id, -1);
     }
+    getEmoji() {
+        return this.props.votes >= 15
+            ? `em em-rolling_on_the_floor_laughing`
+            : this.props.votes >= 12
+              ? `em em-laughing`
+              : this.props.votes >= 9
+                ? `em em-smiley`
+                : this.props.votes >= 6
+                  ? `em em-slightly_smiling_face`
+                  : this.props.votes >= 3 ? `em em-neutral_face` : this.props.votes >= 0 ? `em em-confused` : `em em-angry`;
+    }
 
     render() {
+        const { votes, joke } = this.props;
         return (
             <StyledJoke>
                 <JokeButtons>
                     <UpArrow onClick={this.upVote} />
-                    <JokeVotes>{this.props.votes} </JokeVotes>
+                    <JokeVotes votes={votes}>{votes} </JokeVotes>
                     <DownArrow onClick={this.downVote} />
                 </JokeButtons>
-                <JokeText>{this.props.joke}</JokeText>
+                <JokeText>{joke}</JokeText>
                 <JokeSmiley>
-                    <Smiley />
+                    <Smiley className={this.getEmoji()} />
                 </JokeSmiley>
             </StyledJoke>
         );
