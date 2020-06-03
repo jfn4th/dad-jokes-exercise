13862551a8a1ab84199a957bcb112347ff5e1b51
Jamie Nelson <jfn4th@gmail.com>
2020-06-03 00:02:34 -0500

    Updates

---
+++ precache-manifest.12f17ec683b2021ab2c995d1a9c0ed1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a7c85e464ea5f72fcb85edbb20f000",
+    "url": "/dad-jokes-exercise/index.html"
+  },
+  {
+    "revision": "43098c318610def7185f",
+    "url": "/dad-jokes-exercise/static/css/main.70c47f89.chunk.css"
+  },
+  {
+    "revision": "441f8bfcf710b10716e2",
+    "url": "/dad-jokes-exercise/static/js/2.6dbd7ebe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dad-jokes-exercise/static/js/2.6dbd7ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43098c318610def7185f",
+    "url": "/dad-jokes-exercise/static/js/main.54641a31.chunk.js"
+  },
+  {
+    "revision": "df2a64d6d7bd18ddbe6b",
+    "url": "/dad-jokes-exercise/static/js/runtime-main.892c6f0c.js"
+  }
+]);
